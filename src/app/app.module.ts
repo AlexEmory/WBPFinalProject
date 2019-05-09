4a82a8e6f1ecab25a18225a5b3471ea273062ff9
Alexandra Emory <aemory1@students.towson.edu>
2019-05-09 15:42:21 -0400

    Added imports to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import * as express from 'express';
+import * as morgan from 'morgan';
+import * as bodyParser from 'body-parser';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { response } from 'express';
+import * as dotenv from 'dotenv';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { WeatherComponent } from './weather/weather.component';
+import { Parser } from '@angular/compiler';
 
 @NgModule({
   declarations: [
@@ -14,7 +22,15 @@ import { WeatherComponent } from './weather/weather.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgModule,
+    express,
+    morgan,
+    bodyParser,
+    HttpClient,
+    response,
+    dotenv,
+    Component
   ],
   providers: [],
   bootstrap: [AppComponent]
