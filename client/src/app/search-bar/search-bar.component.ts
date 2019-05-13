480e152cc5b672d2d4ae3fd77af14dfe570e8988
Alexandra Emory <aemory1@students.towson.edu>
2019-05-13 15:24:52 -0400

    Added min and max to html

--- client/src/app/search-bar/search-bar.component.ts
+++ client/src/app/search-bar/search-bar.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import {WeatherComponent} from '../weather/weather.component';
 @Component({
+  providers:[WeatherComponent],
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
